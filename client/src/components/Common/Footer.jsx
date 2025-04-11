981b81ed2dca009bffa4d785aa09245313f6199a
khaja <khajashek99@gmail.com>
2025-04-11 17:52:56 +0530

    first commit

--- client/src/components/Common/Footer.jsx
+++ client/src/components/Common/Footer.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
-import "../../styles/Navbar.css";
+import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaArrowUp } from "react-icons/fa";
+import "../../styles/Navbar.css"; 
 
 const Footer = () => {
   return (
     <footer className="footer-section">
-      <div className="footer-content">
-        <p>&copy; 2025 Farm IT. All rights reserved.</p>
-        <div className="footer-links">
-          <a href="#hero">Home</a>
-          <a href="#features">Features</a>
-          <a href="#about">About</a>
-          <a href="#contact">Contact</a>
+      <div className="footer-wrapper">
+        <div className="footer-left">
+          <p>&copy; 2025 <strong>Farm IT</strong>. All Rights Reserved</p>
+          <span>Designed by <a href="https://github.com/khajavali8/Farmloan1">Khajavali</a></span>
         </div>
-        <div className="social-icons">
-          <a href="#"><FaFacebook /></a>
-          <a href="#"><FaTwitter /></a>
-          <a href="#"><FaInstagram /></a>
+        <div className="footer-right">
+          <div className="social-icons">
+            <a href="#"><FaTwitter /></a>
+            <a href="#"><FaFacebook /></a>
+            <a href="#"><FaInstagram /></a>
+            <a href="#"><FaLinkedin /></a>
+          </div>
+          <a href="#" className="scroll-top"><FaArrowUp /></a>
         </div>
       </div>
     </footer>
